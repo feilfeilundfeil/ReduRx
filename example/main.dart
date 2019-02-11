5c0edb302e7e5ce032f46be25c937144da93c17a
David Wimmer <david.wimmer@ffuf.de>
2019-02-11 14:38:11 +0100

    added afterReduce() to Actions Middleware cna no longer make changes to the state adjusted example and test files to these changes

--- example/main.dart
+++ example/main.dart
@@ -11,7 +11,19 @@ class State {
 class Increment extends Action<State> {
   Increment([this.by = 1]);
   final int by;
-  State reduce(State state) => State(state.count + by);
+  State reduce(state) {
+    print('REDUCE');
+    return State(state.count + by);
+  }
+
+  @override
+  void afterReduce(Store<State> store, State state) {
+    print('AFTERREDUCE');
+    if (state.count < 10) {
+      store.dispatch(Increment(by + 1));
+    }
+    super.afterReduce(store, state);
+  }
 }
 
 void main() {
@@ -25,8 +37,8 @@ void main() {
   // After action: Increment: 1 (from LogMiddleware)
   print(store.state.count); // 1
 
-  store.dispatch(Increment(2));
-  // Before action: Increment: 1 (from LogMiddleware)
-  // After action: Increment: 3 (from LogMiddleware)
-  print(store.state.count); // 3
+//  store.dispatch(Increment(2));
+//  // Before action: Increment: 1 (from LogMiddleware)
+//  // After action: Increment: 3 (from LogMiddleware)
+//  print(store.state.count); // 3
 }
